f53dac330654077bbaa8cf80ed108910db5328c2
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-11-21 20:59:34 +0100

    Use lower prob to increase diameter

--- lua/bench.lua
+++ lua/bench.lua
@@ -8,11 +8,11 @@ local epsilon = arg[3] or 0.1
 local epsilon = { 0.8, 0.2, 0.1 }
 local size = { 1, 10, 30 }
 local filename = {
-	'../data/erdos-renyi/100-0.5p.txt',
-	'../data/erdos-renyi/200-0.5p.txt',
-	--'../data/erdos-renyi/500-0.5p.txt',
-	--'../data/erdos-renyi/800-0.5p.txt',
-	--'../data/erdos-renyi/1k-0.5p.txt',
+	'../data/erdos-renyi/100-0.618p.txt',
+	'../data/erdos-renyi/200-0.618p.txt',
+	'../data/erdos-renyi/500-0.618p.txt',
+	'../data/erdos-renyi/800-0.5p.txt',
+	'../data/erdos-renyi/1k-0.5p.txt',
 	--'../data/erdos-renyi/2k-0.5p.txt',
 	--'../data/erdos-renyi/5k-0.5p.txt',
 	--'../data/erdos-renyi/10k-0.5p.txt',
--- lua/generate-graphs.sh
+++ lua/generate-graphs.sh
@@ -1,22 +1,22 @@
 
-python random-graph.py 100 0.618 > ../data/erdos-renyi/100-0.618p.txt
+python random-graph.py 100 0.01 > ../data/erdos-renyi/100-0.618p.txt
 echo 'Generate 100 nodes graoh.'
-python random-graph.py 200 0.618 > ../data/erdos-renyi/200-0.618p.txt
+python random-graph.py 200 0.01 > ../data/erdos-renyi/200-0.618p.txt
 echo 'Generate 200 nodes graoh.'
-python random-graph.py 500 0.618 > ../data/erdos-renyi/500-0.618p.txt
+python random-graph.py 500 0.01 > ../data/erdos-renyi/500-0.618p.txt
 echo 'Generate 500 nodes graoh.'
 python random-graph.py 800 0.618 > ../data/erdos-renyi/800-0.618p.txt
 echo 'Generate 800 nodes graoh.'
 python random-graph.py 1000 0.618 > ../data/erdos-renyi/1k-0.618p.txt
 echo 'Generate 1k nodes graoh.'
-python random-graph.py 2000 0.618 > ../data/erdos-renyi/2k-0.618p.txt
-echo 'Generate 2k nodes graoh.'
-python random-graph.py 5000 0.618 > ../data/erdos-renyi/5k-0.618p.txt
-echo 'Generate 5k nodes graoh.'
-python random-graph.py 10000 0.618 > ../data/erdos-renyi/10k-0.618p.txt
-echo 'Generate 10k nodes graoh.'
-python random-graph.py 20000 0.618 > ../data/erdos-renyi/20k-0.618p.txt
-echo 'Generate 20k nodes graoh.'
+#python random-graph.py 2000 0.618 > ../data/erdos-renyi/2k-0.618p.txt
+#echo 'Generate 2k nodes graoh.'
+#python random-graph.py 5000 0.618 > ../data/erdos-renyi/5k-0.618p.txt
+#echo 'Generate 5k nodes graoh.'
+#python random-graph.py 10000 0.618 > ../data/erdos-renyi/10k-0.618p.txt
+#echo 'Generate 10k nodes graoh.'
+#python random-graph.py 20000 0.618 > ../data/erdos-renyi/20k-0.618p.txt
+#echo 'Generate 20k nodes graoh.'
 #python random-graph.py 50000 0.5 > ../data/erdos-renyi/50k-0.5p.txt
 #echo 'Generate 50k nodes graoh.'
 #python random-graph.py 100000 0.5 > ../data/erdos-renyi/100k-0.5p.txt
