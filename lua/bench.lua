f6a2697ba5b066e7fa44c515188f2a82abeababc
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-11-25 10:15:41 +0100

    Update the bench Lua script

--- lua/bench.lua
+++ lua/bench.lua
@@ -8,15 +8,24 @@ local epsilon = arg[3] or 0.1
 local epsilon = { 0.8, 0.2, 0.1 }
 local size = { 1, 10, 30 }
 local filename = {
-	'../data/erdos-renyi/100-0.618p.txt',
-	'../data/erdos-renyi/200-0.618p.txt',
-	'../data/erdos-renyi/500-0.618p.txt',
-	'../data/erdos-renyi/800-0.618p.txt',
-	'../data/erdos-renyi/1k-0.618p.txt',
-	--'../data/erdos-renyi/2k-0.5p.txt',
-	--'../data/erdos-renyi/5k-0.5p.txt',
-	--'../data/erdos-renyi/10k-0.5p.txt',
-	--'../data/erdos-renyi/20k-0.5p.txt',
+
+	'../data/erdos-renyi/100-0.001p.txt',
+	'../data/erdos-renyi/200-0.001p.txt',
+	'../data/erdos-renyi/500-0.001p.txt',
+	'../data/erdos-renyi/800-0.001p.txt',
+	'../data/erdos-renyi/1k-0.001p.txt',
+
+	'../data/erdos-renyi/100-0.01p.txt',
+	'../data/erdos-renyi/200-0.01p.txt',
+	'../data/erdos-renyi/500-0.01p.txt',
+	'../data/erdos-renyi/800-0.01p.txt',
+	'../data/erdos-renyi/1k-0.01p.txt',
+
+	'../data/erdos-renyi/100-0.1p.txt',
+	'../data/erdos-renyi/200-0.1p.txt',
+	'../data/erdos-renyi/500-0.1p.txt',
+	'../data/erdos-renyi/800-0.1p.txt',
+	'../data/erdos-renyi/1k-0.1p.txt',
 }
 
 for _, f in pairs (filename) do
