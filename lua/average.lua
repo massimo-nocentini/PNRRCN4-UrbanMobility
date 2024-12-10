f7019d749258d40cca66867fe55fc6e9fadbd4fe
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-12-10 13:06:46 +0100

    bfs

--- lua/average.lua
+++ lua/average.lua
@@ -9,6 +9,18 @@ local nvertices, graph = libluabag.load_binary_repr '/data/bitcoin/bitcoin-webgr
 
 print ('Loaded in ', os.difftime(os.time(), t))
 
+t = os.time ()
+
+local distances = libluabag.bin_bfs (0, nvertices, graph)
+
+print ('bfs in ', os.difftime(os.time(), t))
+
+for k, v in ipairs (distances) do
+
+	print (k, v)
+
+end
+
 libluabag.free_binary_repr (nvertices, graph)
 
 print 'freed.'
