6321031262ffbee1b2ae89660890e0aed8a2d8a1
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-12-17 09:25:29 +0100

    Results for 1k

--- data/erdos-renyi/benchmark.sh
+++ data/erdos-renyi/benchmark.sh
@@ -1,9 +1,20 @@
 
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.8 false > 1k-0001p-08e.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.8 true >> 1k-0001p-08e.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.2 false > 1k-0001p-02e.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.2 true >> 1k-0001p-02e.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.1 false > 1k-0001p-01e.out
+time avgdist-rs ./1k-0001p ./1k-0001p-t 60 0.1 true >> 1k-0001p-01e.out
+
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.8 false > 2k-0001p-08e.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.8 true >> 2k-0001p-08e.out
 time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 false > 2k-0001p-02e.out
 time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.2 true >> 2k-0001p-02e.out
 time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 false > 2k-0001p-01e.out
 time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 true >> 2k-0001p-01e.out
 
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.8 false > 5k-0001p-08e.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.8 true >> 5k-0001p-08e.out
 time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 false > 5k-0001p-02e.out
 time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.2 true >> 5k-0001p-02e.out
 time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 false > 5k-0001p-01e.out
