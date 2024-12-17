caf14a138dc1ae1eac56efca9c33808e8a1153f8
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-12-17 09:11:27 +0100

    Some results

--- data/erdos-renyi/benchmark.sh
+++ data/erdos-renyi/benchmark.sh
@@ -1,3 +1,6 @@
 
 time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 false > 2k-0001p.out
+time avgdist-rs ./2k-0001p ./2k-0001p-t 60 0.1 true >> 2k-0001p.out
+
 time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 false > 5k-0001p.out
+time avgdist-rs ./5k-0001p ./5k-0001p-t 60 0.1 true >> 5k-0001p.out
